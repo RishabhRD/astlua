18cb0581229fddab399d923ef737c0baf2d7abf2
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-07-07 12:29:58 +0530

    simple ast main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
-#include <string>
-#include <vector>
-#include "lexer/skip_non_tokens.hpp"
 #include "parser/ast.hpp"
 
 int main() {
-  std::string const str{"--[[]]f"};
-  auto begin = std::begin(str);
-  auto end = std::end(str);
-  auto itr = lua::lexer::skip_non_tokens(begin, end);
-  if (itr == std::end(str)) {}
+  lua::ast::ast a{};
+  lua::ast::ast b{};
+  if (a == b) {}
 }
