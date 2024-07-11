57a397c3e12f95b4e513ecc69f656f27106d79a5
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-07-11 22:47:53 +0530

    list_parser added

--- include/parser/parse_expresssion.hpp
+++ include/parser/parse_expresssion.hpp
@@ -14,15 +14,19 @@ inline auto name_parser = match_if_then(
       return std::get<token::identifier>(name_token).value;
     });
 
-inline auto comma_name_parser =
-    sequence([](auto, auto name) { return name; },
-             match(token::symbol::COMMA, std::monostate{}), name_parser);
+inline auto list_parser(auto parser) {
+  auto comma_val_parser =
+      sequence([](auto, auto val) { return val; },
+               match(token::symbol::COMMA, std::monostate{}), parser);
 
-inline auto name_list_parser = sequence(
-    [](auto name, auto lst) {
-      return ast::list_1{std::move(name), std::move(lst)};
-    },
-    name_parser, zero_or_more(comma_name_parser));
+  return sequence(
+      [](auto val, auto lst) {
+        return ast::list_1{std::move(val), std::move(lst)};
+      },
+      std::move(parser), zero_or_more(comma_val_parser));
+}
+
+inline auto name_list_parser = list_parser(name_parser);
 
 inline auto number_parser = match_if_then(
     [](auto const& token) {
@@ -45,4 +49,7 @@ inline auto true_parser = match(token::keyword::TRUE, ast::true_t());
 inline auto false_parser = match(token::keyword::FALSE, ast::false_t());
 inline auto vararg_parser = match(token::symbol::VARARG, ast::vararg());
 
+inline auto exp_parser =
+    choice<ast::expr>(number_parser, string_parser, nil_parser, true_parser,
+                      false_parser, vararg_parser);
 }  // namespace lua::parser
--- tests/parser/parse_expression_test.cpp
+++ tests/parser/parse_expression_test.cpp
@@ -11,13 +11,13 @@ namespace parse_expression_test {
 namespace __desc {
 auto pass(std::string_view desc,
           std::vector<lua::token::token_info> const& tokens, auto f,
-          auto const& res, std::size_t len) {
+          auto const& expected, std::size_t len) {
   sub(desc.data()) {
     auto begin = std::begin(tokens);
     auto end = std::end(tokens);
     auto res_opt = f(begin, end);
     req(res_opt.has_value() == true);
-    req(res_opt->first == res);
+    req(res_opt->first == expected);
     req(res_opt->second - begin == len);
   }
 }
@@ -62,41 +62,23 @@ test("name_parser") {
   fail("do", {token::keyword::DO}, name_parser);
 }
 
-test(",name parser") {
-  pass(",name", {token::symbol::COMMA, token::identifier("hello")},
-       comma_name_parser, "hello", 2);
-  pass(",name something",
-       {token::symbol::COMMA, token::identifier("hello"), token::keyword::DO},
-       comma_name_parser, "hello", 2);
-  fail(",", {token::symbol::COMMA}, comma_name_parser);
-  fail("name", {token::identifier("hello")}, comma_name_parser);
-  fail("", {}, comma_name_parser);
-  fail("do", {token::keyword::DO}, comma_name_parser);
+template <typename Node>
+auto test_list_parser(auto parser, auto token, Node const& node) {
+  pass("token", {token}, parser, ast::list_1{node}, 1);
+  pass("token,", {token, token::symbol::COMMA}, parser, ast::list_1{node}, 1);
+  pass("token,token", {token, token::symbol::COMMA, token}, parser,
+       ast::list_1{node, {node}}, 3);
+  pass("token,token,",
+       {token, token::symbol::COMMA, token, token::symbol::COMMA}, parser,
+       ast::list_1{node, {node}}, 3);
+  pass("token,token,token",
+       {token, token::symbol::COMMA, token, token::symbol::COMMA, token},
+       parser, ast::list_1{node, {node, node}}, 5);
 }
 
 test("name list parser") {
-  pass("name", {token::identifier("hello")}, name_list_parser,
-       ast::list_1{std::string{"hello"}}, 1);
-  pass("name,", {token::identifier("hello"), token::symbol::COMMA},
-       name_list_parser, ast::list_1{std::string{"hello"}}, 1);
-  pass("name,name",
-       {token::identifier("hello"), token::symbol::COMMA,
-        token::identifier("world")},
-       name_list_parser,
-       ast::list_1{std::string{"hello"}, std::vector<std::string>{"world"}}, 3);
-  pass("name,name,",
-       {token::identifier("hello"), token::symbol::COMMA,
-        token::identifier("world"), token::symbol::COMMA},
-       name_list_parser,
-       ast::list_1{std::string{"hello"}, std::vector<std::string>{"world"}}, 3);
-  pass(
-      "name,name,name",
-      {token::identifier("hello"), token::symbol::COMMA,
-       token::identifier("world"), token::symbol::COMMA,
-       token::identifier("!")},
-      name_list_parser,
-      ast::list_1{std::string{"hello"}, std::vector<std::string>{"world", "!"}},
-      5);
+  test_list_parser(name_list_parser, token::identifier("hello"),
+                   std::string{"hello"});
 }
 
 test("number_parser") {
